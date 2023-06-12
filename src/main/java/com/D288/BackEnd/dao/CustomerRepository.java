ad6b220abf13730b012067c651ab6a1d6819896e
rpavli5 <rpavli5@wgu.edu>
2023-06-12 00:50:45 -0700

    Part E Created Interfaces for DAO Package that extend JPARepo

---
+++ src/main/java/com/D288/BackEnd/dao/CustomerRepository.java
@@ -0,0 +1,7 @@
+package com.D288.BackEnd.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepository extends JpaRepository {
+}
---
+++ src/main/java/com/D288/BackEnd/dao/ExcursionRepository.java
@@ -0,0 +1,6 @@
+package com.D288.BackEnd.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExcursionRepository extends JpaRepository {
+}
